a34f622a6538286449a0f2fdbae03b1c4fc5b012
gabrielf.04.2002 <gabrielf.04.2002@gmail.com>
2024-10-13 17:31:48 +0000

    remove dependencies that dont use

--- src/main/java/com/example/demo/LoanCheckerController.java
+++ src/main/java/com/example/demo/LoanCheckerController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
